70c0b8a7e46deaf7c59d7b71591868c4e34b4b77
Gonçalo Galante <72885029+goncalogalante@users.noreply.github.com>
2022-09-29 15:55:47 +0100

    adding kfold, cross validation and some comments

--- ml-proj.py
+++ ml-proj.py
@@ -11,9 +11,15 @@ from sklearn.linear_model import BayesianRidge
 from sklearn.linear_model import LassoLars
 from sklearn.linear_model import SGDRegressor
 from sklearn.linear_model import TheilSenRegressor
+from sklearn.linear_model import ElasticNet
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_absolute_error 
 from sklearn.metrics import mean_squared_error as MSE
 from sklearn.metrics import balanced_accuracy_score as BACC
+from sklearn.model_selection import cross_val_score, StratifiedKFold, KFold
 
+## Scores between the y real and y predicted
 def scores(y_real,y_pred,mode):
     ###y_real - ground truth vector 
     ###y_pred - vector of predictions, must have the same shape as y_real
@@ -32,13 +38,47 @@ def scores(y_real,y_pred,mode):
             print('The Balanced Accuracy is', bacc)
             return bacc
         
+        elif mode == 's':
+            sse = np.sum((y_real - y_pred)**2)
+            print('The Sum of Squares Error is', sse)
+            return sse
+        
         else:
             print('You must define the mode input.')
 
+## Validation Scores inside the validation set             
+def model_val_scores(model, x_train_val, x_test_val, y_train_val, y_test_val, mode):
+    
+    model.fit(x_train_val, y_train_val)
+    y_pred = model.predict(x_test_val)
+    
+    if mode == 'r':
+        mse = MSE(y_test_val,y_pred)
+        return mse
+        
+    elif mode == 's':
+        sse = np.sum((y_test_val - y_pred)**2)
+        return sse
+    
+    elif mode == 'sc':
+        return model.score(x_test_val, y_test_val)
+    
+    elif mode == 'r2':
+        return r2_score(y_test_val, y_pred)
+    
+    else:
+        print('You must define the mode input.')
+    
+    
 ## Load datasets
 x_train = np.load('Xtrain_Regression1.npy')
 y_train = np.load('Ytrain_Regression1.npy')
 x_test = np.load('Xtest_Regression1.npy')
+
+## Scalling the data in case of different types of data 
+sc_X = StandardScaler()
+x_train = sc_X.fit_transform(x_train)
+x_test = sc_X.transform(x_test)
 
 ## Evaluate training sets shape
 num_rows_x, num_cols_x = x_train.shape
@@ -46,16 +86,16 @@ num_rows_x, num_cols_x = x_train.shape
 num_rows_y, num_cols_y = y_train.shape
 #print ('Y:', num_rows_y, num_cols_y)
 
-## Define models
+## Define models and fitting the training data on it 
 model = LinearRegression()
 model_LR = model.fit(x_train, y_train)
 
-## Define predictors
+## Define predictors for training set and test ste
 y_train_pred = model.predict(x_train)
 y_pred = model.predict(x_test)
 #print("y_pred:", y_pred)
 
-## Calculate parameters values
+## Evaluate regression parameters values
 param_beta0 = model.intercept_
 param_beta = model.coef_
 
@@ -64,15 +104,32 @@ df = pd.DataFrame({'y_train': y_train.tolist(), 'y_train_pred': y_train_pred.tol
 df.head()
 print(df)
 
-## Calculate SSE and MSE
-sse = np.sum((y_train_pred - y_train)**2)
-print('SSE:', sse)
+## Calculate SSE, MSE, R^2 and MAE
+print('SSE:', scores(y_train,y_train_pred,'s'))
 print('MSE:', scores(y_train,y_train_pred,'r'))
-score = r2_score(y_train, y_train_pred) #regression R^2 score
-print('Score R^2', score)
+R2_score = r2_score(y_train, y_train_pred) #regression R^2 score
+print('R^2:', R2_score) 
+MAE = mean_absolute_error(y_train, y_train_pred)
+print("Mean Absolute Error:", MAE)
 
-#print(param_beta)
-#print(param_beta0)
+"""Validation"""
+## Define KFold validation method 
+kf = KFold(n_splits=5)
+val_scores = []
+
+## KFold Validation for any type of regression and mode defined
+for train_index, test_index in kf.split(x_train):
+    print("TRAIN:", train_index, "\nTEST:", test_index)
+    x_train_val, x_test_val = x_train[train_index], x_train[test_index]
+    y_train_val, y_test_val = y_train[train_index], y_train[test_index]
+    #print("x_train_val | y_train_val\n",len(x_train_val), len(y_train_val), x_train_val, y_train_val)
+    #print("x_test_val | y_test_val\n", len(x_test_val), len(y_test_val), x_test_val, y_test_val)
+    val_scores.append(model_val_scores(LinearRegression(), x_train_val, x_test_val, y_train_val, y_test_val, 's'))
+print("\n K Folds Validation Scores method:", val_scores)
+
+## Cross Validation for any type of regression
+print("\n Cross Validation Scores method:", cross_val_score(LinearRegression(), x_train, y_train, cv= 3))
+
 
 #print(df['y_train'])
 
