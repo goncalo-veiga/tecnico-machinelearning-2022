134d0a4319d5b4e3da0293ac7fd1c66eeaa3d317
goncalo-veiga <goncalo_miguel_veiga@hotmail.com>
2022-10-04 12:52:27 +0100

    close #4: final touches for first delivery

--- ml-proj.py
+++ ml-proj.py
@@ -32,11 +32,7 @@ def scores(y_real,y_pred,mode):
             mse = MSE(y_real,y_pred)
             print('The Mean Square Error is', mse)
             return mse
-        
-        elif mode == 'c':
-            bacc = BACC(y_real,y_pred)
-            print('The Balanced Accuracy is', bacc)
-            return bacc
+
         
         elif mode == 's':
             sse = np.sum((y_real - y_pred)**2)
@@ -69,7 +65,6 @@ def model_val_scores(model, x_train_val, x_test_val, y_train_val, y_test_val, mo
     else:
         print('You must define the mode input.')
     
-    
 ## Load datasets
 x_train = np.load('Xtrain_Regression1.npy')
 y_train = np.load('Ytrain_Regression1.npy')
@@ -82,9 +77,7 @@ x_test = sc_X.transform(x_test)
 
 ## Evaluate training sets shape
 num_rows_x, num_cols_x = x_train.shape
-#print ('X:', num_rows_x, num_cols_x)
 num_rows_y, num_cols_y = y_train.shape
-#print ('Y:', num_rows_y, num_cols_y)
 
 ## Define models and fitting the training data on it 
 model = Ridge(alpha=0.06)
@@ -93,7 +86,6 @@ model_LR = model.fit(x_train, y_train)
 ## Define predictors for training set and test ste
 y_train_pred = model.predict(x_train)
 y_pred = model.predict(x_test)
-#print("y_pred:", y_pred)
 
 ## Evaluate regression parameters values
 param_beta0 = model.intercept_
@@ -106,6 +98,7 @@ df.head()
 print(df)
 
 ## Calculate SSE, MSE, R^2 and MAE
+print("-------Without Validation Results-------")
 print('SSE:', scores(y_train,y_train_pred,'s'))
 print('MSE:', scores(y_train,y_train_pred,'r'))
 R2_score = r2_score(y_train, y_train_pred) #regression R^2 score
@@ -114,46 +107,43 @@ MAE = mean_absolute_error(y_train, y_train_pred)
 print("Mean Absolute Error:", MAE)
 
 """Validation"""
+print("-------Validation Set Results-------")
 ## Define KFold validation method 
 kf = KFold(n_splits=4)
 val_scores = []
 
 ## KFold Validation for any type of regression and mode defined
 for train_index, test_index in kf.split(x_train):
-    print("TRAIN:", train_index, "\nTEST:", test_index)
+    #print("TRAIN:", train_index, "\nTEST:", test_index)
     x_train_val, x_test_val = x_train[train_index], x_train[test_index]
     y_train_val, y_test_val = y_train[train_index], y_train[test_index]
-    #print("x_train_val | y_train_val\n",len(x_train_val), len(y_train_val), x_train_val, y_train_val)
-    #print("x_test_val | y_test_val\n", len(x_test_val), len(y_test_val), x_test_val, y_test_val)
-    val_scores.append(model_val_scores(LinearRegression(), x_train_val, x_test_val, y_train_val, y_test_val, 's'))
-print("\n K Folds Validation Scores method:", val_scores)
+    val_scores.append(model_val_scores(Ridge(alpha=0.06), x_train_val, x_test_val, y_train_val, y_test_val, 's'))
 
-## Cross Validation for any type of regression
-#print("\n Cross Validation Scores method:", cross_val_score(LinearRegression(), x_train, y_train, cv= 3))
+
+print("K Folds Validation Scores method:", val_scores)
+print("SSE Score:",np.mean(val_scores))
 
 ## Calculate ideal alpha
-ridge_cv = RidgeCV(alphas = [0.05,0.06,0.08,0.09,0.1,0.11,0.12,0.13,0.14,0.15,0.16,0.17]).fit(x_train,y_train)
+ridge_cv = RidgeCV(alphas = [0.05,0.06,0.08,0.09,0.1]).fit(x_train,y_train)
 
 ## Calculate best split number
 min_score = 0
 min_step = 0
 for step in range(2,50):
-    print("Now testing step:",step)
+    #print("Now testing step:",step)
     my_values = cross_val_score(Ridge(alpha=0.06), x_train, y_train, cv = step)
     my_score = np.mean(my_values)
     if my_score > min_score:
         min_score = my_score
         min_step = step
 
-print("min score:", min_score, "split:", min_step)
-print("\n Cross Validation Scores method:", cross_val_score(Ridge(alpha=0.06), x_train, y_train, cv = min_step))
+#print("Min Score:", min_score, "split:", min_step)
+
+print("Cross Validation Scores method:", cross_val_score(Ridge(alpha=0.06), x_train, y_train, cv = min_step))
 
 #score
 print("The train score for ridge model is {}".format(ridge_cv.score(x_train, y_train)))
-print("alpha OP:", ridge_cv.alpha_)
+print("Best alpha:", ridge_cv.alpha_)
 
-#print(df['y_train'])
-
-#sns.lmplot(data=df, x="y_train", y="y_train_pred")
 
 
