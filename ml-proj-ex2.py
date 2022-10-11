57f8ea81dfa08b08d1b2e1b66ffc7c8af7dc6a7f
Gonçalo Galante <72885029+goncalogalante@users.noreply.github.com>
2022-10-11 22:56:29 +0100

    adding outlier algorithms: LOF, IF, EE

--- ml-proj-ex2.py
+++ ml-proj-ex2.py
@@ -17,9 +17,11 @@ from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_squared_error as MSE
 from sklearn.metrics import balanced_accuracy_score as BACC
 from sklearn.model_selection import cross_val_score, StratifiedKFold, KFold
-import time
 
-from openpyxl.workbook import Workbook
+from sklearn.neighbors import LocalOutlierFactor
+from sklearn.ensemble import IsolationForest
+from sklearn.covariance import EllipticEnvelope
+
 
 ## Scores between the y real and y predicted
 def scores(y_real,y_pred,mode):
@@ -68,8 +70,8 @@ x_train = np.load('Xtrain_Regression2.npy')
 y_train = np.load('Ytrain_Regression2.npy')
 x_test = np.load('Xtest_Regression2.npy')
 
-print(x_train)
-print(y_train)
+#print(x_train)
+#print(y_train)
 ## Converting
 """## convert your array into a dataframe
 #df = pd.DataFrame (x_train)
@@ -81,14 +83,12 @@ filepath1 = 'ex2_ydata.xlsx'
 df1.to_excel(filepath1, index=False)"""
 
 """Analysing the Data"""
-
 raw_data = pd.read_csv('ex2_dataset.csv')
-
 x_features = ['x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7', 'x8', 'x9', 'x10']
 y = ['y']
 
 ## Data distribution plot
-for i in x_features:
+"""for i in x_features:
     x_values = raw_data[i].values
     mean = raw_data[i].mean()
     sns.displot(x_values, color = 'red', kind = 'kde', legend = i)
@@ -103,8 +103,55 @@ for j in x_features:
 
 ## Data pair plot    
 pairplot = sns.pairplot(raw_data, plot_kws={'color':'green'})
-plt.show()
+plt.show()"""
 
 
 
+""" Outlier Algorithms and Validation """
 
+## Local Outlier Factor Algorithm
+print("------ Local Outlier Factor ------")
+
+clf_LFO = LocalOutlierFactor(n_neighbors=20, contamination=0.2)
+clf_fp = clf_LFO.fit_predict(x_train)
+x_train_new = []
+for i in range (0, len(clf_fp)):
+    if clf_fp[i] == -1:
+        #print("Outlier Line:", i, x_train[i])
+        print("Outlier line:", i)
+    else:
+        x_train_new.append(x_train[i])
+
+x_train_new = np.array(x_train_new)
+
+## Isolation Forest Algorithm
+print("------ ISOLATION FOREST ------")
+
+clf_IF = IsolationForest(contamination = 0.2)
+clf_IF_fp = clf_IF.fit_predict(x_train)
+x_train_new = []
+
+for i in range (0, len(clf_IF_fp)):
+    if clf_IF_fp[i] == -1:
+        #print("Outlier Line:", i, x_train[i])
+        print("Outlier line:", i)
+    else:
+        x_train_new.append(x_train[i])
+
+x_train_new = np.array(x_train_new)
+
+## Eliptical Envellope Algorithm
+print("------ ELIPTICAL ENVELLOPE ------")
+
+clf_EE = EllipticEnvelope(contamination=0.2)
+clf_EE_fp = clf_EE.fit_predict(x_train)
+x_train_new = []
+
+for i in range (0, len(clf_EE_fp)):
+    if clf_EE_fp[i] == -1:
+        #print("Outlier Line:", i, x_train[i])
+        print("Outlier line:", i)
+    else:
+        x_train_new.append(x_train[i])
+
+x_train_new = np.array(x_train_new)
