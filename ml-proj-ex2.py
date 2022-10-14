f0206911f6d7d1b9bcdaa0b777d174ef5aa778b3
Gonçalo Galante <72885029+goncalogalante@users.noreply.github.com>
2022-10-14 16:48:34 +0100

    adding normalization function

--- ml-proj-ex2.py
+++ ml-proj-ex2.py
@@ -1,6 +1,5 @@
-from pyexpat import model
+
 from sklearn.preprocessing import normalize
-from time import time
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -15,13 +14,10 @@ from sklearn.linear_model import BayesianRidge
 from sklearn.linear_model import LassoLars
 from sklearn.linear_model import SGDRegressor
 from sklearn.preprocessing import StandardScaler
-from sklearn.svm import OneClassSVM
 from sklearn.metrics import r2_score
 from sklearn.metrics import mean_absolute_error 
 from sklearn.metrics import mean_squared_error as MSE
-from sklearn.metrics import balanced_accuracy_score as BACC
-from sklearn.model_selection import cross_val_score, StratifiedKFold, KFold
-from sklearn.model_selection import RepeatedKFold
+from sklearn.model_selection import cross_val_score, StratifiedKFold, KFold, RepeatedKFold
 from sklearn.neighbors import LocalOutlierFactor
 from sklearn.ensemble import IsolationForest
 from sklearn.covariance import EllipticEnvelope
@@ -31,35 +27,26 @@ from sklearn.linear_model import TheilSenRegressor
 
 from pyod.models.ecod import ECOD
 from pyod.models.knn import KNN 
-from pyod.utils.data import generate_data
-from pyod.utils.data import evaluate_print
-from pyod.utils.example import visualize
-pd.set_option('display.max_rows', None)
 
-## Scores between the y real and y predicted
+## This function returns MSE, SSE and R^2 scores
 def scores(y_real,y_pred,mode):
     
-    if mode == 'r':
+    if mode == 'm':
         mse = MSE(y_real,y_pred)
         print('The Mean Square Error is', mse)
         return mse
-    
-    elif mode == 'c':
-        bacc = BACC(y_real,y_pred)
-        print('The Balanced Accuracy is', bacc)
-        return bacc
     
     elif mode == 's':
         sse = np.sum((y_real - y_pred)**2)
         print('The Sum of Squares Error is', sse)
         return sse
             
-## Validation Scores inside the validation set             
+## This function returns the scores for the validation set              
 def model_val_scores(model, x_train_val, x_test_val, y_train_val, y_test_val, mode):
     
     model.fit(x_train_val, y_train_val)
     y_pred = model.predict(x_test_val)
-    
+
     if mode == 'm':
         mse = MSE(y_test_val,y_pred)
         return mse
@@ -76,6 +63,7 @@ def model_val_scores(model, x_train_val, x_test_val, y_train_val, y_test_val, mo
     
     else:
         print('You must define the mode input.')
+        
     
 ## Load datasets
 x_train = np.load('Xtrain_Regression2.npy')
@@ -87,11 +75,26 @@ raw_data = pd.read_csv('ex2_dataset.csv')
 x_features = ['x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7', 'x8', 'x9', 'x10']
 y = ['y']
 
-## Outlier Algorithms and Validation
+## Data Processing
+
 x_train = normalize(x_train)
 y_train = normalize(y_train)
 x_train2 = np.delete(x_train,[72,34,48,63,71,96,39,5,15,20,26,41,58,93,29,51,61,73,80,99],axis=0)
 y_train2 = np.delete(y_train,[72,34,48,63,71,96,39,5,15,20,26,41,58,93,29,51,61,73,80,99],axis=0)
+
+#print(x_train)
+def normalization(data):  
+    
+    feat_mean = np.mean(data,axis=0)
+    n=0
+    for row in data:
+        i=0
+        for value in row:
+            data[n][i] = value - feat_mean[i]
+            i += 1
+        n += 1    
+        
+normalization(x_train)
 """ Boxplot outliers
 df_xtrain = pd.DataFrame(x_train,columns=['x1','x2','x3','x4','x5','x6','x7','x8','x9','x10'])
 print(df_xtrain.head())
@@ -101,13 +104,16 @@ print(df1['x10'])
 sns.boxplot(data = df1)
 plt.show()
 """
-## Local Outlier Factor Algorithm
-print("------ Local Outlier Factor ------")
+## Outlier Detection Algorithms
+
+## Local Outlier Factor Algorithm 
+
 model_LOF = LocalOutlierFactor(n_neighbors=20, contamination=0.2)
 model_LOF_outliers = model_LOF.fit_predict(x_train)
 x_train_LOF = []
 y_train_LOF = []
 outlier_index_LOF = []
+## Remove outliers
 for i in range (0, len(model_LOF_outliers)):
     if model_LOF_outliers[i] == -1:
         #print("Outlier Line:", i, x_train[i])
@@ -119,15 +125,16 @@ for i in range (0, len(model_LOF_outliers)):
 
 x_train_LOF = np.array(x_train_LOF)
 y_train_LOF = np.array(y_train_LOF)
+print("> Local Outlier Factor: Outliers detected and removed.", outlier_index_LOF)
 
 ## Isolation Forest Algorithm
-print("------ ISOLATION FOREST ------")
 
 model_IF = IsolationForest(contamination = 0.2,random_state=0)
 model_IF_outliers = model_IF.fit_predict(x_train)
 x_train_IF = []
 y_train_IF = []
 outlier_index_IF = []
+## Remove outliers
 for i in range (0, len(model_IF_outliers)):
     if model_IF_outliers[i] == -1:
         #print("Outlier Line:", i, x_train[i])
@@ -139,15 +146,16 @@ for i in range (0, len(model_IF_outliers)):
 
 x_train_IF = np.array(x_train_IF)
 y_train_IF = np.array(y_train_IF)
+print("> ISOLATION FOREST: Outliers detected and removed.", outlier_index_IF)
 
 ## Eliptical Envellope Algorithm
-print("------ ELIPTICAL ENVELLOPE ------")
 
 model_EE = EllipticEnvelope(contamination=0.2,random_state=0)
 model_EE_outliers = model_EE.fit_predict(x_train)
 x_train_EE = []
 y_train_EE = []
 outlier_index_EE = []
+## Remove outliers
 for i in range (0, len(model_EE_outliers)):
     if model_EE_outliers[i] == -1:
         #print("Outlier Line:", i, x_train[i])
@@ -159,30 +167,31 @@ for i in range (0, len(model_EE_outliers)):
 
 x_train_EE = np.array(x_train_EE)
 y_train_EE = np.array(y_train_EE)
+print("> ELIPTICAL ENVELLOPE: Outliers detected and removed.", outlier_index_EE)
 
 ## Find common outliers
 common_outliers = {}
 all_outliers = outlier_index_LOF + outlier_index_IF + outlier_index_EE
-print(" all outliers", all_outliers)
+#print(" All detected outliers:", all_outliers)
 for line in all_outliers:
     if line in common_outliers:
         common_outliers[line] += 1
     else:
         common_outliers[line] = 1
 
-print("Common outliers:",dict(sorted(common_outliers.items(), key=lambda item: item[1], reverse=True)))
+#print("Common outliers:",dict(sorted(common_outliers.items(), key=lambda item: item[1], reverse=True)))
 
+## This function evaluates the different types of Regression methods with Kfolds 
 def kfold_func(x_train,y_train):
-    ## KFold validation method for any type of regression 
     n_splits = 10
     kf = KFold(n_splits, shuffle = False)
     Ridge_val_scores = []
     LinearR_val_scores = []
     Lasso_val_scores = []
     ridge_cv = RidgeCV(alphas = [0.5,1,1.5,1.7,1.8,1.82,1.83,1.84,1.85,1.86,1.87,1.88,1.89,1.9,1.95,2,2.05,2.1,2.16,2.17,2.18,2.19,2.2,2.21,2.22,2.23,2.25,2.27,2.3,2.4,2.5,2.6,3,3.2,3.3,3.4,3.5,3.6,4,5,6]).fit(x_train,y_train)
-    print("alpha Ridge",ridge_cv.alpha_)
+    print("alpha Ridge:",ridge_cv.alpha_)
     lasso_cv = LassoCV(alphas = [1100,1200,1300,1400,1500,2000,2200,2300,2500,3000,5000,7000,10000,15000,1000000]).fit(x_train,y_train)
-    print("alpha lasso",lasso_cv.alpha_)
+    print("alpha Lasso:",lasso_cv.alpha_)
     
     for train_index, test_index in kf.split(x_train):
         x_train_val, x_test_val = x_train[train_index], x_train[test_index]
@@ -200,8 +209,19 @@ def kfold_func(x_train,y_train):
         print("-> Best is Linear Regression:", minSSE)
     else:
         print("-> Best is Lasso Regression:", minSSE)
+        
+print("----- Local Outlier Factor SSE-----")
+kfold_func(x_train_LOF,y_train_LOF.ravel())
+print("----- Isolation Forest SSE-----")
+kfold_func(x_train_IF,y_train_IF.ravel())
+print("----- Eliptical Envellope SSE-----")
+kfold_func(x_train_EE,y_train_EE.ravel())
+print("----- Removed most common outliers SSE -----")
+kfold_func(x_train2,y_train2.ravel())
 
-def kfold_rob_func(x_train,y_train):
+
+## This functions returns the scores for Robust Regressors such as Huber, RANSAC and Thei-sen
+"""def kfold_rob_func(x_train,y_train):
     ## KFold validation method for any type of regression 
     kf = KFold(10, shuffle = False)
     Huber_val_scores = []
@@ -217,7 +237,7 @@ def kfold_rob_func(x_train,y_train):
     
     min = 9999
     ideal_alpha = 99999
-    """
+    
     for my_alpha in range(1,1300):
         
         for train_index, test_index in kf.split(x_train):
@@ -227,18 +247,9 @@ def kfold_rob_func(x_train,y_train):
         if np.average(Huber_val_scores) < min:
             min = np.average(Huber_val_scores)
             ideal_alpha = my_alpha
-    """
+    
     print("idela alpha:",ideal_alpha,"SSE:")
-    print("Score: SSE | K Folds Validation nº",10,"splits | Huber:", np.average(Huber_val_scores),"| Ransac:", np.average(Ransac_val_scores),"| Theilsen:", np.average(Theilsen_val_scores))
-
-print("----LOF SSE----")
-kfold_func(x_train_LOF,y_train_LOF.ravel())
-print("----IF SSE----")
-kfold_func(x_train_IF,y_train_IF.ravel())
-print("----EE SSE----")
-kfold_func(x_train_EE,y_train_EE.ravel())
-print("----Removed most common SSE----")
-kfold_func(x_train2,y_train2.ravel())
+    print("Score: SSE | K Folds Validation nº",10,"splits | Huber:", np.average(Huber_val_scores),"| Ransac:", np.average(Ransac_val_scores),"| Theilsen:", np.average(Theilsen_val_scores))"""
 
 """
 model_huber = OneClassSVM(kernel = 'sigmoid',nu=0.201)
@@ -256,3 +267,4 @@ print("counter:",count)
 print("----Huber SSE----")
 kfold_rob_func(normalize(x_train),normalize(y_train))
 """
+
