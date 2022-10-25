c0ec87413efffc732cb11afe15d4aff34165f5ad
Gonçalo Galante <72885029+goncalogalante@users.noreply.github.com>
2022-10-25 12:21:15 +0100

    adding optimizations and a better cnn

--- mlclass1_.py
+++ mlclass1_.py
@@ -1,5 +1,7 @@
 
+from audioop import avg
 import sys
+from tabnanny import verbose
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -19,10 +21,20 @@ from keras.applications.resnet import ResNet50
 from tensorflow import keras
 from keras import optimizers
 from keras.optimizers import schedules
+import os
+from sklearn.utils import shuffle
+from imblearn.over_sampling import SMOTE, RandomOverSampler
+os.environ["CUDA_VISIBLE_DEVICES"]="-1"
+import tensorflow as tf
 
 np.set_printoptions(threshold=sys.maxsize)
-pd.set_option('display.max_rows', None)
+#pd.set_option('display.max_rows', None)
 warnings.filterwarnings('ignore')
+
+#config = tf.ConfigProto(
+##        device_count = {'GPU': 0}
+#    )
+#sess = tf.Session(config=config)
 
 
 def recall_m(y_true, y_pred):
@@ -45,17 +57,20 @@ def f1_score(y_true, y_pred):
 ## Load datasets
 x_train = np.load('Xtrain_Classification1.npy')
 y_train = np.load('ytrain_Classification1.npy')
-#x_test = np.load('Xtest_Regression2.npy')
+x_test = np.load('Xtest_Classification1.npy')
 
 ## Evaluate shape
 print(y_train.shape)
 print(x_train.shape)
+print(x_test.shape)
 
 ## Dataset processing (array to image) and saves the images
 x_train_new = []
-count_eyespot = 0
-count_spot = 0
+x_test_new = []
+count_eyespot_train = 0
+count_spot_train = 0
 
+## TRAINING SET
 for i in range(0, len(x_train)):
     x_train_new.append(x_train[i].reshape(30,30,3))
     #img  = Image.fromarray(x_train_new[i])
@@ -64,19 +79,28 @@ for i in range(0, len(x_train)):
         #print("EYESPOTS indices:", i)
         #img  = Image.fromarray(x_train_new[i])
         #img.save("eyespots/%d.png"%(i))
-        count_eyespot += 1
+        count_eyespot_train += 1
     else:
         #print("SPOTS indice:", i)
         #img  = Image.fromarray(x_train_new[i])
         #img.save("spots/%d.png"%(i))
-        count_spot += 1
+        count_spot_train += 1
+        
+## TEST SET
+for i in range(0, len(x_test)):
+    x_test_new.append(x_test[i].reshape(30,30,3))
+    #img  = Image.fromarray(x_test_new[i])
+    #img.save("testset/%d.png"%(i))
         
 print("The Images are saved successfully")         
-print("Nº of Eyespots", count_eyespot)
-print("Nº of Spots:", count_spot)
-    
-x_train_new = np.array(x_train_new)
+print("TRAIN SET | Nº of Eyespots", count_eyespot_train)
+print("TRAIN SET |Nº of Spots:", count_spot_train)       
 
+x_train_new = np.array(x_train_new)
+print(x_train_new.shape)
+
+x_test_new = np.array(x_test_new)
+print(x_test_new.shape)
 ## Solving Imbalanced Dataset (eyespots: 3131 | spot: 5142)
 
 # - 1º Approach: Undesampling - eyespots: 3131 | spot: 3131
@@ -84,20 +108,56 @@ x_train_new = np.array(x_train_new)
 
 # - 2º Approach: Oversampling - eyespots: 5142 (3131 + 2011) | spot: 5142
 
+df_y_train = pd.DataFrame({'y_train': y_train.tolist()})
+df_x_train = pd.DataFrame({'x_train': x_train_new.tolist()})
+print(df_y_train.value_counts())
+
+smote = RandomOverSampler(sampling_strategy='minority')
+x_oversampling, y_oversampling = smote.fit_resample(x_train, df_y_train)
+
+print("Nº DE SPOTS E EYESPOTS:", y_oversampling.value_counts())
+y_oversampling = np.array(y_oversampling)
+
+x_oversampling_new = []
+for i in range(0, len(x_oversampling)):
+    x_oversampling_new.append(x_oversampling[i].reshape(30,30,3))
+    if y_oversampling[i] == 1:
+        #print("EYESPOTS indices:", i)
+        img  = Image.fromarray(x_oversampling_new[i])
+        img.save("oversampling/eyespots/%d.png"%(i))
+        count_eyespot_train += 1
+    else:
+        #print("SPOTS indice:", i)
+        img  = Image.fromarray(x_oversampling_new[i])
+        img.save("oversampling/spots/%d.png"%(i))
+        count_spot_train += 1
+ 
+#x_oversampling_new, y_oversampling = shuffle(x_oversampling_new, y_oversampling, random_state = 0) 
+print("gravacao done")  
+print("Y OVER", y_oversampling)
+x_oversampling_new = np.array(x_oversampling_new)
+print(x_oversampling_new.shape)
+
+x_oversampling_new, y_oversampling = shuffle(x_oversampling_new, y_oversampling)
 
 # - 3º Approach: Data Augmentation - eyespots: ~~ 6500 | spot: ~~ 6500     
 
-     
-## Normalization
+## Normalization and shuffling
 x_train_new = x_train_new/255
 
-## Callback function to advise when there is no improvement for accuracy
-callback = keras.callbacks.EarlyStopping(monitor='val_acc', patience=2, verbose=1)
+x_oversampling_new = x_oversampling_new/255
 
-## Model CNN 
+##  Convolutional Neural Network Model
+callback = keras.callbacks.EarlyStopping(monitor='val_acc', patience=8, verbose=1)
+# This callback will stop the training when there is no improvement in
+# the loss for 2 consecutive epochs.
+
+print("VETOR DO Y", y_oversampling)
 
 model = models.Sequential()
-# 1º layer convolutional
+
+## FIRST CNN
+"""# 1º layer convolutional
 model.add(layers.Conv2D(64, (5, 5), activation='relu', input_shape=(30, 30, 3)))
 # add a pooling layer
 model.add(layers.MaxPooling2D(pool_size=(2, 2)))
@@ -107,9 +167,9 @@ model.add(layers.Conv2D(64, (5, 5), activation='relu'))
 model.add(layers.MaxPooling2D(pool_size=(2, 2)))
 
 # Flattening layer
-model.add(layers.Flatten())
+model.add(layers.Flatten()) 
 
-# add a layer with 500 neurons
+# add a layer with 500 neurons))
 model.add(layers.Dense(500, activation='relu'))
 # drop out layer
 model.add(layers.Dropout(0.5))
@@ -119,27 +179,56 @@ model.add(layers.Dense(250, activation='relu'))
 model.add(layers.Dropout(0.5))
 # add another layer with 125 neurons
 model.add(layers.Dense(125, activation='relu'))
-
+# drop out layer
 model.add(layers.Dropout(0.6))
-
 # add another layer with 75 neurons
 model.add(layers.Dense(75, activation='relu'))
-
 # add the last layer with 1 neurons
-model.add(layers.Dense(1, activation='sigmoid'))
+model.add(layers.Dense(1, activation='sigmoid'))"""
+## ----------------------------------------------------------------
+
+## SECOND CNN
+model.add(layers.Conv2D(32, (5,5), padding = 'same', activation = 'relu', input_shape=(30,30,3)))
+model.add(layers.MaxPooling2D())
+model.add(layers.Dropout(0.2))
+model.add(layers.Conv2D(64, (5,5), padding = 'same', activation = 'relu'))
+model.add(layers.MaxPooling2D())
+model.add(layers.Dropout(0.2))
+model.add(layers.Flatten())
+model.add(layers.Dense(128, activation='relu'))
+model.add(layers.Dropout(0.4))
+model.add(layers.Dense(1, activation = 'sigmoid'))
+
+
+## Third CNN
+"""model.add(layers.Conv2D(64, (6,6), padding = 'same', activation = 'relu', input_shape=(30,30,3)))
+model.add(layers.Conv2D(64, (6,6), padding = 'same', activation = 'relu'))
+model.add(layers.MaxPooling2D(padding = 'same'))
+model.add(layers.Conv2D(64, (3,3), padding = 'same', activation = 'relu'))
+model.add(layers.Conv2D(64, (3,3), padding = 'same', activation = 'relu'))
+model.add(layers.MaxPooling2D(padding = 'same'))
+#model.add(layers.Conv2D(128, (5,5), padding = 'same', activation = 'relu'))
+#model.add(layers.MaxPooling2D(padding = 'same'))
+model.add(layers.Flatten())
+model.add(layers.Dense(1254, activation='relu'))
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(1, activation = 'sigmoid'))"""
 
 model.summary()
 
-model.compile(optimizer = 'adam', loss = 'binary_crossentropy',metrics = ['acc', f1_score])
-hist = model.fit(x_train_new, y_train, batch_size=376, epochs=10, validation_split=0.2, callbacks = callback)
+
+model.compile(optimizer = optimizers.Adam() , loss = 'binary_crossentropy',metrics = ['acc'])
+hist = model.fit(x_oversampling_new, y_oversampling, batch_size=64, epochs=25, validation_split=0.16, callbacks = [callback])
 
 acc = hist.history['acc']
 val_acc = hist.history['val_acc']
 loss = hist.history['loss']
 val_loss = hist.history['val_loss']
-## Note: EArly STOPING PREVENT OVERFITTING, callback
-epochs = range(1, len(acc) + 1)
+#f1_acc = hist.history['f1_score']
+#val_f1_acc = hist.history['val_f1_score']
 
+epochs = range(1, len(acc) + 1)
+## Traning and Validation Accuracy
 plt.plot(epochs, acc, 'bo', label='Training acc')
 plt.plot(epochs, val_acc, 'r', label='Validation acc')
 plt.title('Training and Validation accuracy')
@@ -147,6 +236,7 @@ plt.legend()
 
 plt.figure()
 
+## Training and Validation Loss
 plt.plot(epochs, loss, 'bo', label='Training loss')
 plt.plot(epochs, val_loss, 'r', label='Validation loss')
 plt.title('Training and Validation loss')
@@ -154,4 +244,12 @@ plt.legend()
 
 plt.show()  
 
-## convert y into one hot enconding
+## Training and Validation Accuracy
+#plt.plot(epochs, f1_acc, 'bo', label='F1 score Training acc')
+#plt.plot(epochs, val_f1_acc, 'r', label='F1 score Validation acc')
+#plt.title('F1 score Training and Validation acc')
+#plt.legend()
+
+#plt.show()
+
+## convert y into one hot enconding 
